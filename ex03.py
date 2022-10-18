c47352522d3fc8524fc2200904aa195970c91189
euna <eunaoo@gmail.com>
2022-10-18 15:05:56 +0900

    quiz03변수출력

--- ex03.py
+++ ex03.py
@@ -2,3 +2,6 @@ num1=10
 num2=20
 print('{:^20}' .format('---출력 결과---'))
 print('num1({}) + num2({}) = {}' .format(num1,num2,num1+num2))
+
+print('num1(',num1,') + num2(',num2,') = ',num1+num2)
+print(f'num1({num1}) + num2({num2}) = {num1+num2}')
