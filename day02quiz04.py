11a00fd71bb426ccd25ee14d7dd6789251c4cd0d
euna <eunaoo@gmail.com>
2022-10-19 14:30:05 +0900

    input 형변환

---
+++ day02quiz04.py
@@ -0,0 +1,13 @@
+print("두 수의 합을 구해 줍니다")
+n1=int(input('첫번째 수 입력 : '))
+n2=int(input('두번째 수 입력 : '))
+n3=n1+n2
+print(f'두 수의 합 {n1} + {n2} = {n3}')
+
+print('\n')
+
+print("두 수의 합을 구해 줍니다")
+n4=input('첫번째 수 입력 : ')
+n5=input('두번째 수 입력 : ')
+n6=int(n4)+int(n5)
+print('두 수의 합 {} + {} = {}'.format(n4,n5,n6))
